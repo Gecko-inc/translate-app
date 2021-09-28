9f3ebb7dc16e3f454959a4ddc3fc6052879cb87f
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2021-09-28 17:50:01 +0300

    interface

--- static/js/main.js
+++ static/js/main.js
@@ -7,9 +7,9 @@ start.innerHTML = 'Start';
 let stop = document.createElement('button');
 stop.id = 'stop';
 stop.innerHTML = 'Stop';
-document.body.appendChild(div);
-document.body.appendChild(start);
-document.body.appendChild(stop);
+document.querySelector('#foiceRec').appendChild(div);
+document.querySelector('#foiceRec').appendChild(start);
+document.querySelector('#foiceRec').appendChild(stop);
 navigator.mediaDevices.getUserMedia({ audio: true})
     .then(stream => {
         const mediaRecorder = new MediaRecorder(stream);
@@ -52,3 +52,63 @@ async function sendVoice(form) {
         document.querySelector('#messages').appendChild(audio);
     }
 }
+
+// Typing
+let typingContainer = document.querySelector('#typingText')
+
+let typewriter = new Typewriter(typingContainer, {
+    loop: true,
+    delay: 75,
+});
+  
+typewriter
+    .pauseFor(100)
+    .typeString('Выберите язык')
+    .pauseFor(2500)
+    .deleteChars(13)
+    .typeString('Select a language')
+    .pauseFor(2500)
+    .deleteChars(17)
+    .typeString('Tildi tańdańyz')
+    .pauseFor(2500)
+    .start();
+
+// Select lenguage
+selectKazahstan.addEventListener('click', function(){
+    selectLenguage.style.display = "none";
+    app.style.display = "block";
+ 
+    document.querySelector('#languageKZ').style.color = "#59BECC"
+    document.querySelector('.hint-title').append('Sálem');
+    document.querySelector('.hint-subtitle').append('Bul qalaı jumys isteıdi ?');
+
+    document.querySelector('.hint-one').append('Mıkrofondy basyńyz');
+    document.querySelector('.hint-two').append('"Sálem Oleg" dep aıtyńyz jáne ol sizge bárin aıtady');
+    document.querySelector('.hint-three').append('Eger ýaqyt bolmasa, "Oleg [sózdi] [aǵylshyn] tiline aýdaryńyz" dep aıtyńyz.');
+});
+
+selectRussian.addEventListener('click', function(){
+    selectLenguage.style.display = "none";
+    app.style.display = "block";
+
+    document.querySelector('#languageRU').style.color = "#59BECC"
+    document.querySelector('.hint-title').append('Привет');
+    document.querySelector('.hint-subtitle').append('Как это работает ?');
+
+    document.querySelector('.hint-one').append('Нажимай на микрофон');
+    document.querySelector('.hint-two').append('Скажи "Привет Олег" и он тебе сам все расскажет');
+    document.querySelector('.hint-three').append('Если нет времени, просто скажи "Олег переведи [слово] на [английский язык]"');
+});
+
+selectEngl.addEventListener('click', function(){
+    selectLenguage.style.display = "none";
+    app.style.display = "block";
+
+    document.querySelector('#languageUS').style.color = "#59BECC"
+    document.querySelector('.hint-title').append('Hello');
+    document.querySelector('.hint-subtitle').append('How does it work?');
+
+    document.querySelector('.hint-one').append('Press the microphone');
+    document.querySelector('.hint-two').append('Say "Hi Oleg" and he\'\ll tell you all about it.');
+    document.querySelector('.hint-three').append('If there is no time, just say "Oleg, translate [word] into [English].');
+});
