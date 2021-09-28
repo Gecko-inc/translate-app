5fd5a5926450dc75b120054f717bd72720de4b6e
Nicstim <56487341+nicstim@users.noreply.github.com>
2021-09-28 16:54:08 +0300

    fix recognition

--- section/views.py
+++ section/views.py
@@ -1,6 +1,8 @@
+import subprocess
 import os
 import time
 
+from django.conf import settings
 from django.core.files.storage import default_storage
 from django.http import JsonResponse
 from django.views.generic import TemplateView
@@ -21,14 +23,21 @@ class Index(TemplateView):
     @classmethod
     def translete_audio(cls, request):
         file = request.FILES['voice']
-        file_name = default_storage.save(f"{str(time.time()).replace('.', '')}{file.name}.wav", file)
-        file = default_storage.open(file_name)
-        print(default_storage.url(file_name))
+        file_name = f'{str(time.time()).replace(".", "")}.wav'
+        file_name_out = f'{str(time.time()).replace(".", "1")}.wav'
+
+        with open(file_name, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+        process = subprocess.run(['ffmpeg', '-i', file_name, file_name_out])
+        if process.returncode != 0:
+            raise Exception("Что-то пошло не так")
         rec = sr.Recognizer()
-        sample_audio = sr.AudioFile(file)
+        sample_audio = sr.AudioFile(file_name_out)
         with sample_audio as audio_file:
             audio_content = rec.record(audio_file)
             txt = rec.recognize_google(audio_content, language="ru-RU")
-            print(txt)
-        os.remove(default_storage.url(file_name))
-        return JsonResponse({"msg": True})
+        os.remove(file_name)
+        os.remove(file_name_out)
+        print(txt)
+        return JsonResponse({"msg": txt})
