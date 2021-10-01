32ef5b8db647ec86ec997092470e561950d50766
Nicstim <56487341+nicstim@users.noreply.github.com>
2021-10-01 23:13:01 +0300

    translate.geckostudio.xyz

--- django_template/settings.py
+++ django_template/settings.py
@@ -26,7 +26,7 @@ PARSER_API = config['parser_api']
 
 # При False необходимо настроить nginx
 DEBUG = True
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = ["*", 'translate.geckostudio.xyz']
 
 # Используемые приложения
 INSTALLED_APPS = [
