9650c923a4629787d9a16d4694cb1a761b7522bd
Nicstim <56487341+nicstim@users.noreply.github.com>
2021-10-02 17:15:18 +0300

    fix

--- django_template/urls.py
+++ django_template/urls.py
@@ -23,10 +23,7 @@ urlpatterns += i18n_patterns(
     prefix_default_language=False
 )
 
-if DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += [
-        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-        url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    ]
+urlpatterns += [
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
